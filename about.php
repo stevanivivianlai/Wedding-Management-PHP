55bfdf5cdb86b48a8341cc80a67dd4616d8681a3
stevanivivianlai <91085835+stevanivivianlai@users.noreply.github.com>
2023-10-20 10:42:37 +0700

    44

--- about.php
+++ about.php
@@ -1,7 +1,132 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ACLC
- * Date: 2/18/2018
- * Time: 5:21 PM
- */
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Wedding Planner</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdn.materialdesignicons.com/2.1.19/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/datepicker.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/carousel.css">
+    <style>
+        body {
+            font-family: 'Open Sans', 'Roboto', sans-serif;
+            line-height: 1.5em;
+            margin-bottom: 2%;
+            margin-top: 3%;
+            width: 100%;
+            overflow-x: hidden;
+            background: #f1f1f1;
+        }
+
+        .navbar-light .navbar-brand {
+            color: #1a1a1a;
+            font-weight: bold;
+            line-height: 22px;
+        }
+
+        .navbar {
+            font-weight: 700;
+            padding: 12px;
+            font-style: normal;
+            font-size: 14px;
+            text-transform: uppercase;
+            color: black;
+            border-bottom: 1px solid #ddd;
+        }
+
+        li.nav-item > a.nav-link {
+            color: black !important;
+            font-weight: bold !important;
+        }
+
+        #review {
+            font-size: 16px;
+            font-weight: bold;
+            margin-right: 5px;
+        }
+
+        .form-inline > a.mr-2, .btn.btn-sm.my-2.my-sm-0 {
+            color: black;
+            font-size: 14px;
+            font-weight: 700;
+            margin-left: 10px;
+        }
+
+        .form-inline > a.mr-2:hover, .btn.btn-sm.my-2.my-sm-0:hover {
+            color: #17b4bc;
+            text-decoration: none;
+        }
+
+        a.btn.btn-sm.my-2.my-sm-0.mr-2.loginbtn {
+            background: #dc3545;
+            font-size: 14px;
+            color: white;
+            padding: 5px;
+            border: 2px solid transparent;
+            width: 85px;
+        }
+
+        a.btn.btn-sm.my-2.my-sm-0.mr-2.loginbtn:hover {
+            background: white;
+            border: 2px solid #dc3545;
+            color: #dc3545;
+        }
+
+        .navbar-expand-lg .navbar-nav .nav-link {
+            padding-right: .9rem;
+        }
+
+        .navbar-brand {
+            margin-left: 20px;
+            width: 200px;
+        }
+
+        .form-control {
+            font-size: 12px;
+            border-radius: 0;
+            margin-top: 0;
+        }
+
+
+
+    </style>
+</head>
+<body>
+<?php include "include/nav.php"; ?>
+<section class="section contact-area">
+		<div class="container">
+			<div class="row">
+
+					<div class="col-sm-12">
+					<div class="contact-form margin-bottom">
+						<h2 class="center-text">Kami Membuat Impian Pernikahan Anda Menjadi Kenyataan!</h2>
+						<form method="post">
+							<div class="row">
+								<p class="center-text">Kami adalah tim yang berdedikasi untuk membantu Anda merencanakan dan melaksanakan pernikahan impian Anda. Dengan pengalaman kami yang luas dalam industri pernikahan, kami siap membantu Anda menghadirkan momen-momen yang tak terlupakan di hari istimewa Anda. </p>
+								
+							</div><!-- row -->
+						</form>
+					</div><!-- contact-form -->
+				</div><!-- col-sm-6 -->
+	
+
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- section -->
+
+
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-3.2.1.slim.min.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
